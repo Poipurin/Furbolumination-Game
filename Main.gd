2b77497721593ba23868440171522ccff1af2387
Poipurin <b.pinorojo@gmail.com>
2023-02-12 16:25:00 -0300

    main scene

--- Main.gd
+++ Main.gd
@@ -2,35 +2,31 @@ extends Node
 
 export(PackedScene) var mob_scene
 var score
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	new_game()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_ScoreTimer_timeout():
 	score += 1
-
+	$HUD.update_score(score)
+	
 func _on_StartTimer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
+	
 
 
 func _on_MobTimer_timeout():
