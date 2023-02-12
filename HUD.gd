b7774a86667de044a3f377967bd027b6412fba07
Poipurin <b.pinorojo@gmail.com>
2023-02-12 20:20:01 -0300

    detalles del hud

--- HUD.gd
+++ HUD.gd
@@ -14,7 +14,7 @@ func show_game_over():
 	# Wait until the MessageTimer has counted down.
 	yield($MessageTimer, "timeout")
 
-	$Message.text = "Survive as\n you can"
+	$Message.text = "Reach the \n furb-o-lumination"
 	$Message.show()
 	# Make a one-shot timer and wait for it to finish.
 	yield(get_tree().create_timer(1), "timeout")
